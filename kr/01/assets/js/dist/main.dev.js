54257a484fe356b5eda1ae7cbc13fec450843228
Dmytro <dmytro.palianytsia@gmail.com>
2021-04-19 03:51:25 +0300

    Exam 1

---
+++ kr/01/assets/js/dist/main.dev.js
@@ -0,0 +1,19 @@
+"use strict";
+
+$(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 700) {
+      $("header").addClass("fixed");
+    } else {
+      $("header").removeClass("fixed");
+    }
+  });
+  $(".hamburger, #menu_shadow").click(function () {
+    $(".hamburger").toggleClass("is-active");
+    $("body").toggleClass("open");
+  });
+  $(".mobile_menu a").click(function () {
+    $(".hamburger").removeClass("is-active");
+    $("body").removeClass("open");
+  });
+});
