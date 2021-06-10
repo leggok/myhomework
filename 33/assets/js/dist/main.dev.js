f907ec6e8f220d03abc722b7610d0ee214578e7f
Dmytro <dmytro.palianytsia@gmail.com>
2021-06-11 00:16:10 +0300

    add Lesson 33

---
+++ 33/assets/js/dist/main.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(function () {
+  $('.slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>'
+  });
+});
---
+++ 33/assets/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slider').slick({
+    slidesToShow: 4, 
+    slidesToScroll: 1, 
+    arrows: true,
+    dots: false,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>'
+    });
+});
